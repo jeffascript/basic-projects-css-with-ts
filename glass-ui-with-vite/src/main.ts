f55e9b409d3ab4184fbb376430f7945070e1fd0b
jeffascript <ofoaro.jeff@gmail.com>
2021-06-13 21:08:31 +0200

    features/color-added + typography

--- glass-ui-with-vite/src/main.ts
+++ glass-ui-with-vite/src/main.ts
@@ -1,6 +1,6 @@
 // import './style.css';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+const app = document.querySelector<HTMLDivElement>('#app')!; //#app
 
 app.innerHTML = `
  <p> Hello world</p>
